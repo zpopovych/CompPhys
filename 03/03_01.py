539179f19a0e91e88a1dfc8a41282a4994c164c5
zpopovych <zpopovych@gmail.com>
2021-02-24 11:12:28 -0600

    Assignment 3 started

---
+++ 03/03_01.py
@@ -0,0 +1,58 @@
+""" From "COMPUTATIONAL PHYSICS", 3rd Ed, Enlarged Python eTextBook  
+    by RH Landau, MJ Paez, and CC Bordeianu
+    Copyright Wiley-VCH Verlag GmbH & Co. KGaA, Berlin;  Copyright R Landau,
+    Oregon State Unv, MJ Paez, Univ Antioquia, C Bordeianu, Univ Bucharest, 2015.
+    Support by National Science Foundation
+    
+    adapted by Lev Kaplan in 2019 for linear decay fit y(x) = a + b x """
+ 
+from pylab  import*
+
+x = range(5,120,10)    # time from 5 to 115 in steps of 10 (12 points)
+Nd = len(x)   # number of data points
+y = log([32,17,21,7,8,6,5,2,2,0.1,4,1])   # log of number of counts
+sig = [1] * 12   # error bars all set to 1
+
+print("Number of data points:", Nd)
+
+sig = [1] * Nd   # error bars all set to 1
+
+plot(x, y, 'bo', label="original data" )                                   # Plot data in blue
+
+errorbar(x,y,sig)                                     # Plot error bars
+title('Linear Least Squares Fit')                        # Plot figure
+xlabel( 't [ns]' )                                            # Label axes
+ylabel( 'log(N)' )
+grid(True)                                               # plot grid
+xlim(0,120)                                              # x range for plot
+
+
+ss = sx = sxx = sy = sxy = 0   # initialize various sums
+
+for i in range(0, Nd):         # compute various sums over data points                              
+    sig2 = sig[i] * sig[i]
+    ss += 1. / sig2;    sx += x[i]/sig2;        sy += y[i]/sig2
+    sxx += x[i] * x[i]/sig2;    sxy += x[i]*y[i]/sig2;
+         
+delta = ss*sxx-sx*sx;
+slope = (ss*sxy-sx*sy) / delta    #calculate best fit slope
+inter = (sxx*sy-sx*sxy) / delta   # calculate best fit intercept
+      
+print('Linear Fit Final Results\n') 
+print('y(x) = a + b x')                          # Desired fit
+print('a = ', inter, '+/-', sqrt(sxx/delta))                  
+print('b = ', slope, '+/-', sqrt(ss/delta))
+print('correlation =',-sx/sqrt(sxx*ss))
+
+print('tau = ', -slope, '+/-', sqrt(ss/delta))
+
+
+
+# red line is the fit, red dots the fits at y[i]
+t = range(0,120,1)
+curve  = inter + slope*t
+points = inter + slope*x
+plot(t, curve,'r', label="OLS fit" )
+plot(x, points, 'ro')
+legend(loc="lower left")
+show()
