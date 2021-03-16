5b3866f9f594d1863907fbdb4e8a471a55789d9a
zpopovych <zpopovych@gmail.com>
2021-03-15 19:50:37 -0500

    Problem 5 v error

--- 04/04_05.py
+++ 04/04_05.py
@@ -14,6 +14,7 @@ max = 2**16   # max number of points
 nlist = []
 values = []
 
+
 y = 0.0
 n = 1
 
@@ -31,12 +32,27 @@ for i in range(0,max+1):
 theor = np.empty(len(nlist))
 theor.fill(155/6)
 
+error_values = np.abs(values - theor)
+theor_error = 10 - np.array(nlist)/2
+
+figure(figsize=(5,10))
+subplot(2,1,1)
+
 plot(nlist, values, label = 'Monte-Carlo numerical estimation')
 plot(nlist, theor, label='theoretical value 155/6', alpha=0.5)
+
 title('Monte-Carlo Estimation of Multidimentional Integral \n' +
       r'$\int_0^1 dx_1 \int_0^1 dx_2 ... \int_0^1 dx_{10} \left(x_1 + x_2 + ... + x_{10}\right)^2$')
 ylabel('Value of integral ')
-xlabel(r'$log_{10} ( N )$')
+#xlabel(r'$log_{10} ( N )$')
 legend()
-savefig('04_05_multi_int.png')
+
+subplot(2,1,2)
+plot(nlist, error_values, label='actual error', alpha=0.5)
+plot(nlist, theor_error, '--', label=r'theoretical error trend $1/\sqrt{N}$', alpha=0.5)
+ylabel('Error ')
+xlabel(r'$log_{2} ( N )$')
+legend()
+
+savefig('04_05_multi_int_error.png')
 show()
